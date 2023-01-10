119cdd123b693896d38dabb029ff7d7241c5a301
idakrahl <122284856+idakrahl@users.noreply.github.com>
2023-01-10 22:13:08 +0100

    Add files via upload

---
+++ pilot.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", start);
+
+function start() {
+  const video = document.querySelector("#video_file");
+  const playBtn = document.querySelector("#play");
+  const stopBtn = document.querySelector("#stop");
+  const playBckRate = document.querySelector("#current_rate");
+  const volSlider = document.querySelector("#volControl");
+
+  playBtn.addEventListener("click", playVideo);
+  stopBtn.addEventListener("click", stopVideo);
+  volSlider.addEventListener("change", changeVolume);
+
+  function playVideo() {
+    if (video.paused === true) {
+      video.play();
+      playBtn.style.backgroundImage = "url(t5_indhold/pilotsite/billeder/pause.svg)";
+    } else {
+      video.pause();
+      playBtn.style.backgroundImage = "url(t5_indhold/pilotsite/billeder/play.svg)";
+    }
+  }
+  function stopVideo() {
+    video.pause();
+    video.currentTime = 0;
+    playBtn.style.backgroundImage = "url(t5_indhold/pilotsite/billeder/play.svg)";
+  }
+}
